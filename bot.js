2b76c0a1c73122fce89804d3c3b86edc0d6567f6
maks12345 <50026114+TheMiksHacker@users.noreply.github.com>
2019-07-31 19:09:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Бот запущен, с ${client.users.size} пользователями, в ${client.channels.size} каналах в ${client.guilds.size} серверах.`);
   console.log(`Вход выполнен под ${client.user.tag}!`);
-  client.user.setPresence({ status: 'online', game: { name: '.хелп | ' + client.guilds.size + " серверов" } });
+  client.user.setPresence({ status: 'online', game: { name: 'Let\'s crash | ' + client.guilds.size + " серверов" } });
   client.generateInvite(["ADMINISTRATOR"]).then(link =>{
     console.log(`Ссылка для приглашение бота: ${link}`);
   });
